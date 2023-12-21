615412aece5d68d911917922b34b933dbd514a81
Jesse Nelson <jesse@swirldslabs.com>
2023-12-21 16:53:56 -0600

    testing restart
    Signed-off-by: Jesse Nelson <jesse@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/scripts/init-temp-schema.sh
+++ hedera-mirror-importer/src/main/resources/db/scripts/init-temp-schema.sh
@@ -3,7 +3,9 @@ set -e
 TEMP_SCHEMA="${HEDERA_MIRROR_IMPORTER_DB_TEMPSCHEMA:-temporary}"
 SCHEMA_EXISTS="$(psql -d "user=postgres connect_timeout=3" \
                   -XAt \
-                  -c "select exists (select schema_name from information_schema.schemata where schema_name = '$TEMP_SCHEMA')")"
+                  -c "select exists (select schema_name from information_schema.schemata where schema_name = 'temporary')")"
+
+echo "The schema exists value is $SCHEMA_EXISTS"
 
 if [[ $SCHEMA_EXISTS == 't' ]]
 then
